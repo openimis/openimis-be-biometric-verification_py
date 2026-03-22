b8b85ddb9ccb31749ada1a218b6b899ee80d634d
maxime ngoe <maxime.ngoe@gmail.com>
2026-03-22 21:42:23 +0100

    fix: :bug: Fir URL bug on websocket call

--- biometric_verification/routing.py
+++ biometric_verification/routing.py
@@ -10,5 +10,5 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'api/ws/biometric/verify/$', consumers.BiometricVerificationConsumer.as_asgi()),
+    re_path(r'^api/ws/biometric/verify/$', consumers.BiometricVerificationConsumer.as_asgi()),
 ]
